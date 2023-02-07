58cdf836e16bea9cb91260b19ac27755ec1a4c89
star-min <zxzx00262@gmail.com>
2023-02-07 23:45:22 +0900

    2023/02/07

--- 10988_팰린드롬인지확인하기.py
+++ 10988_팰린드롬인지확인하기.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 a = input().rstrip()
-# a[::-1] = 뒤집어서 출력
+# a[::-1] = 뒤집어서 출력 콜론 마다 시작과 끝 을 삽입할수 있으나 비어있으면 전부 뒤집는다.
 if a == a[::-1]:
     print(1)
 else:
---
+++ 2908_상수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+a, b = input().split()
+
+a = int(a[::-1])
+b = int(b[::-1])
+'''
+if a > b:
+    print(a)
+else:
+    print(b)
+'''
+print(max(a, b))
